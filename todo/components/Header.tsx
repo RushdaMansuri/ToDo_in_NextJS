27cafc586152c2920d8ce9276eae6c52acad40d1
rushdavahora <mansurirushda7@gmail.com>
2024-05-03 22:24:11 -0400

    Added Header and Footer

---
+++ todo/components/Header.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '../styles/Header.module.css'; 
+
+const Header: React.FC = () => {
+  return (
+    <header className={styles.header}>
+      <h1>Todo App</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/todos">Todos</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ todo/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../app/layout';
+
+const HomePage: React.FC = () => {
+  return (
+    <Layout>
+      <h1>Welcome to My Todo App</h1>
+      <p>This is the home page.</p>
+    </Layout>
+  );
+};
+
+export default HomePage;
