4a729b670f1e2d4e3548e6f6331092ca303f929e
rushdavahora <mansurirushda7@gmail.com>
2024-05-04 21:56:17 -0400

    Added homepage

--- todo/app/layout.tsx
+++ todo/app/layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import styles from '../styles/Home.module.css';
 
 interface LayoutProps {
   children: ReactNode;
@@ -8,11 +9,13 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
+    <body className={styles.body}>
     <div>
       <Header />
       <main>{children}</main>
       <Footer />
     </div>
+    </body>
   );
 };
 
--- todo/components/Header.tsx
+++ todo/components/Header.tsx
@@ -4,9 +4,9 @@ import styles from '../styles/Header.module.css';
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <h1>Todo App</h1>
+      <h1 className={styles.todoApp}>Todo App</h1>
       <nav>
-        <ul>
+        <ul className={styles.navList}>
           <li><a href="/">Home</a></li>
           <li><a href="/todos">Todos</a></li>
           <li><a href="/about">About</a></li>
