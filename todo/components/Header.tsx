08875d0ee8855213409da59bedc9e23b34917b1c
rushdavahora <mansurirushda7@gmail.com>
2024-05-05 16:27:25 -0400

    Added image

--- todo/components/Header.tsx
+++ todo/components/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Image from 'next/image'; 
 import styles from '../styles/Header.module.css'; 
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.todoApp}>Todo App</h1>
+      <div className={styles.logoContainer}>
+        {/* Use the next/Image component */}
+        <Image
+          src="/todo.png" // Adjust the path as needed
+          alt="Todo App Icon"
+          width={1000} // Set the desired width
+          height={1000} // Set the desired height
+          className={styles.todoIcon}
+        />
+      </div>
       <nav>
         <ul className={styles.navList}>
           <li><a href="/">Home</a></li>
