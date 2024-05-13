0139477ba0902cd8c521a6a6065044c7e895feae
rushdavahora <mansurirushda7@gmail.com>
2024-05-12 21:57:01 -0400

    Added Edit from in list

--- todo/components/TodoList.tsx
+++ todo/components/TodoList.tsx
@@ -1,6 +1,7 @@
 // TodoList.tsx
 import React, { useState } from 'react';
 import AddTodoForm from './AddTodoForm';
+import EditTodoForm from './EditTodoForm';
 import styles from '../styles/TodoList.module.css';
 
 type Todo = {
@@ -11,6 +12,7 @@ type Todo = {
 
 const TodoList: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [editTodoId, setEditTodoId] = useState<number | null>(null);
 
   const addTodo = (text: string) => {
     const newTodo: Todo = {
@@ -33,6 +35,23 @@ const TodoList: React.FC = () => {
     setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id));
   };
 
+  const editTodo = (id: number) => {
+    setEditTodoId(id);
+  };
+
+  const saveEditedTodo = (id: number, newText: string) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, text: newText } : todo
+      )
+    );
+    setEditTodoId(null);
+  };
+
+  const cancelEdit = () => {
+    setEditTodoId(null);
+  };
+
   return (
     <div className={styles.container}>
       <h1>ToDo List:</h1>
@@ -47,7 +66,20 @@ const TodoList: React.FC = () => {
               onChange={() => toggleTodo(todo.id)}
               className={styles.checkbox}
             />
-            <span className={todo.completed ? styles.completed : undefined}>{todo.text}</span>
+            {editTodoId === todo.id ? (
+              <EditTodoForm
+                initialValue={todo.text}
+                onSave={(text: string) => saveEditedTodo(todo.id, text)}
+                onCancel={cancelEdit}
+              />
+            ) : (
+              <span
+                className={todo.completed ? styles.completed : undefined}
+                onDoubleClick={() => editTodo(todo.id)}
+              >
+                {todo.text}
+              </span>
+            )}
             <button onClick={() => deleteTodo(todo.id)} className={styles.deleteButton}>
               Delete
             </button>
